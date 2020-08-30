b0279eecbad284fdd68ced4a931929c55bcb6443
Deeptanshu Sharma <38813374+Deeptanshu15@users.noreply.github.com>
2020-08-30 21:37:15 +0530

    Added name and about
    Name and a Description(About) added to the user profile profile(this component). Added functionality to display user profile when clicked on the component (onClick method). also added functions to trim name and description.

--- src/components/You.js
+++ src/components/You.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function You() {
+function You(props) {
+
+    const [name] = useState('Deeptanshu Sharma')
+    const [about] = useState('Your description about you or status here')
+
+    var displayName
+    if(name.length>10)
+        displayName = name.substring(0,9)+'...'
+    else
+        displayName = name
+
+
+    var displayAbout
+    if(about.length>10)
+        displayAbout = about.substring(0,50)+'...'
+    else
+        displayAbout = about
+
+    
     return (
-        <div className="you">
+        <div className="you" onClick={() => props.setContact({id: null, name: name, desc: about})}>
+            
             <img src="images/photo-you.jpeg" className="your-img" alt="profile" />
+
             <div className="your-details">
-                <h2>You</h2>
+                <h2>{displayName}</h2><br />
+                <span>(YOU)</span><br />
+                <div className="displayAbout">{displayAbout}</div>
             </div>
             
         </div>
