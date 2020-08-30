03163afed039d7c5b42ea5c7c921888839ef3f7f
Deeptanshu Sharma <38813374+Deeptanshu15@users.noreply.github.com>
2020-08-30 21:48:56 +0530

    Display Chat on click on the contact
    Added Array map to iterate over people (contacts) Array coming through props. Added name and about of contacts (coming through props). Also Added functionality to display corresponding conversation of the contact in chat area, when clicked on the contact card.

--- src/components/People.js
+++ src/components/People.js
@@ -1,32 +1,23 @@
 import React from 'react'
 
-function People() {
+function People(props) {
+
     return (
         <div className="people-list">
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
-            <div className="people">
-                People
-            </div>
+
+            {props.contacts.map(c => 
+
+                <div key={c.id} className="people" onClick={() => props.setContact(c)}>
+
+                    <img src="images/photo-you.jpeg" className="contact-img" alt="profile" />
+
+                    <div className="contact-detail">
+                        <span className="contact-name">{c.name}</span>
+                        <div className="contact-about">{c.desc}</div>
+                    </div>
+                    
+                </div>)}
+            
         </div>
     )
 }
