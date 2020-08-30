799e8dae530cd200dab1634a4a3c25f068d240d2
Deeptanshu Sharma <deeptanshusharma15@yahoo.in>
2020-08-31 02:18:51 +0530

    Updates

---
+++ precache-manifest.08022a7e00e6486f224a1d6b7fc926a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65236c795e57b5641786cc6a52e1667b",
+    "url": "/zomantum-frontend-chat/index.html"
+  },
+  {
+    "revision": "67cd00f9e753d1ec42ee",
+    "url": "/zomantum-frontend-chat/static/css/main.69f417e0.chunk.css"
+  },
+  {
+    "revision": "f92f61028ef3ff1bd084",
+    "url": "/zomantum-frontend-chat/static/js/2.76fe5839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zomantum-frontend-chat/static/js/2.76fe5839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cd00f9e753d1ec42ee",
+    "url": "/zomantum-frontend-chat/static/js/main.4aaad74a.chunk.js"
+  },
+  {
+    "revision": "a730631c838dc99f2fdb",
+    "url": "/zomantum-frontend-chat/static/js/runtime-main.5e257743.js"
+  }
+]);
