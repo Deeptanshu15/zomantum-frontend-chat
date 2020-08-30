a44c04f0d3456b585b8f2d7c8ea47747dcc61ee6
Deeptanshu Sharma <38813374+Deeptanshu15@users.noreply.github.com>
2020-08-30 22:05:09 +0530

    Display selected contact information in chat
    Display currently selected contact information, name, description(about) etc. (received from parent through props) at the top of chat area. Also a function to trim description.

--- src/components/ChatTop.js
+++ src/components/ChatTop.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-function ChatTop() {
+function ChatTop(props) {
+    
+    var desc
+    if(props.contact.desc.length>80)
+        desc = props.contact.desc.substring(0,80)+'...'
+    else
+        desc = props.contact.desc
+
+
     return (
         <div className="chat-top">
             <img src="images/photo-you.jpeg" className="chat-img" alt="profile" />
-            <div className="chat-person">chat-top<br /><div>jhdkjshkajhsdkjahdkjahds</div></div>
+            <div className="chat-person">{props.contact.name}</div><br />
+            <span>{desc}</span>
             
         </div>
     )
