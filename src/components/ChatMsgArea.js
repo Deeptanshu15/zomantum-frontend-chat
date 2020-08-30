d050a3ee68280a5378a15938a97631d2615b2be1
Deeptanshu Sharma <38813374+Deeptanshu15@users.noreply.github.com>
2020-08-30 23:07:00 +0530

    component to compose and send message
    Input to Compose message and button to send message.
    The typed message get added(appended) to the conversation(message list) when the button is pressed, and is displayed at bottom in main chat area. (as you mentioned in the assignment).
     React hooks state management is used to maintain input state.

--- src/components/ChatMsgArea.js
+++ src/components/ChatMsgArea.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function ChatMsgArea() {
+function ChatMsgArea(props) {
+
+    const [msg, setMsg] = useState('')
+
+    const handleChange = (e) => {
+        setMsg(e.target.value)
+        console.log(e.target.value)
+    }
+
     return (
         <div className="chat-msg-area">
-            <input type="text" name="msg" placeholder="Type a message" className="msg-input" />
+            <input type="text" onChange={handleChange} name="msg" value={msg} placeholder="Type a message" className="msg-input" />
+            <button onClick={() => {setMsg('');props.sendMsg(msg)}} className="send-btn">GO</button>
         </div>
     )
 }
